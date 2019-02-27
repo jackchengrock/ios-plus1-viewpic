1e42ec3c658daf7483255ef31dd691c8d4fb37c4
Ｊack Cheng <jackcheng@ackde-macbook-pro.local>
2019-02-27 20:45:44 +0800

    Initial Commit

---
+++ ios-plus1-viewpic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-plus1-viewpic
+//
+//  Created by Jack Cheng on 2019/2/27.
+//  Copyright © 2019 jackrock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
